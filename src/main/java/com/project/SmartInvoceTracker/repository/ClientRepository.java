7c53e85105113c8f9e8ccb53fad46ef06b9c99b3
Neelanshi Rai <neelanshi.as@gmail.com>
2025-07-19 21:03:52 +0530

    Remove Repository
    Removing Repo currently

--- src/main/java/com/project/SmartInvoceTracker/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.SmartInvoceTracker.repository;
-
-import com.project.SmartInvoceTracker.entity.client.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
